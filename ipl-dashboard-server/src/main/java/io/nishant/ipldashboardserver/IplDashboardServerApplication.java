cdb10f70b5df1855a7aa7bfd8c2119535cacc52d
FIRST_NAME LAST_NAME <nishant@navigus.in>
2021-04-14 23:25:47 +0530

    initial setup

---
+++ ipl-dashboard-server/src/main/java/io/nishant/ipldashboardserver/IplDashboardServerApplication.java
@@ -0,0 +1,13 @@
+package io.nishant.ipldashboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardServerApplication.class, args);
+	}
+
+}
---
+++ ipl-dashboard-server/src/test/java/io/nishant/ipldashboardserver/IplDashboardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nishant.ipldashboardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
